a6b88a088fa177bea75c3cbc9636472e9e78f1dd
whoenig44 <will.hoenig44@gmail.com>
2024-12-27 07:51:05 -0600

    Made a few changes and testing re-deployment of site on push to main branch

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome to the Health Monitoring Application!!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Push to main branch of Git Repo to trigger rebuild of Production Application
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more about the React framework and Vite Web Bundler
       </p>
     </>
   )
