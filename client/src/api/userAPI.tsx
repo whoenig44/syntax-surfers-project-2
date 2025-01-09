f336d1c741dfe89e92aeb9facf592d33d026a4e1
ShannonJTaylor <shannonjo8475@gmail.com>
2025-01-09 18:46:03 -0500

    Added graph seed data.

--- client/src/api/userAPI.tsx
+++ client/src/api/userAPI.tsx
@@ -1,25 +1,25 @@
-// import Auth from '../utils/auth';
+import Auth from '../utils/auth';
 
-// const retrieveUsers = async () => {
-//   try {
-//     const response = await fetch('/api/users', {
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Authorization: `Bearer ${Auth.getToken()}`
-//       }
-//     });
-//     const data = await response.json();
+const retrieveUsers = async () => {
+  try {
+    const response = await fetch('/api/users', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${Auth.getToken()}`
+      }
+    });
+    const data = await response.json();
 
-//     if(!response.ok) {
-//       throw new Error('Invalid user API response, check network tab!');
-//     }
+    if(!response.ok) {
+      throw new Error('Invalid user API response, check network tab!');
+    }
 
-//     return data;
+    return data;
 
-//   } catch (err) { 
-//     console.log('Error from data retrieval:', err);
-//     return [];
-//   }
-// }
+  } catch (err) { 
+    console.log('Error from data retrieval:', err);
+    return [];
+  }
+}
 
-// export { retrieveUsers };
+export { retrieveUsers };
