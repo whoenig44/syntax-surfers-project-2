23598128e6d66239713f9ae591410b9e2523336a
whoenig44 <will.hoenig44@gmail.com>
2025-01-13 09:57:05 -0500

    Removing extra src_code file from the repo

--- client_src_code/interfaces/UserLogin.tsx
+++
@@ -1,5 +0,0 @@
-// Interface definition for UserLogin
-export interface UserLogin {
-  username: string | null;  // Property for storing username, nullable
-  password: string | null;  // Property for storing password, nullable
-}
