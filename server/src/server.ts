081be915dc1923870790ee15e1a3c0587327c2e1
whoenig44 <will.hoenig44@gmail.com>
2025-01-13 11:47:52 -0500

    Updated file paths in server.ts trying to get routes to work on deployed site

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
 app.use(cors());
-app.use(express.static('../client/dist'));
+app.use(express.static('client/dist'));
 
 app.use(express.json());
 app.use(routes);
