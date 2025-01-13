f019311ea9a9f6c114b1f7f13dcb466c148af57e
AmySuperCoder <tribegriffith@gmail.com>
2025-01-13 12:27:03 -0500

    trying to deploy changed static path for server

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,13 +7,18 @@ dotenv.config();
 import express from 'express';
 import sequelize from './config/connection.js';
 import routes from './routes/index.js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
 app.use(cors());
-app.use(express.static('client/dist'));
+app.use(express.static(path.join(__dirname, '../../client/dist')))
 
 app.use(express.json());
 app.use(routes);
