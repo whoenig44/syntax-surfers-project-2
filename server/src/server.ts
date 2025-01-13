565885226a9753997ea08bd4409a975b8136ee13
AmySuperCoder <tribegriffith@gmail.com>
2025-01-13 12:39:16 -0500

    server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,10 +18,11 @@ const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../../client/dist')))
+
 
 app.use(express.json());
 app.use(routes);
+app.use(express.static(path.join(__dirname, '../../client/dist')))
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
